a16fd35654b5cab2bf64de74677e30e344d93fb8
lucas <metalhead@localhost.localdomain>
2019-11-08 21:34:53 -0300

    Put the attributes in the script

---
+++ script/main.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent('my-event', {
+
+    init: function () {
+        let fish = document.querySelector('#fish');
+        fish.setAttribute('position', '-.5 2 -4');
+        fish.setAttribute('rotation', '0 -90 0');
+        fish.setAttribute('scale', '.26');
+        fish.setAttribute('material', 'src: ../models/fish.jpg');
+    }
+
+});
