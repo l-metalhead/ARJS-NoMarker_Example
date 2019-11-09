8116712b928250737288a902e1c9eb5593330094
lucas <metalhead@localhost.localdomain>
2019-11-08 21:47:30 -0300

    Trying to fix fish invisible issue 01

--- script/main.js
+++ script/main.js
@@ -4,7 +4,7 @@ AFRAME.registerComponent('my-event', {
         let fish = document.querySelector('#fish');
         fish.setAttribute('position', '-.5 2 -4');
         fish.setAttribute('rotation', '0 -90 0');
-        fish.setAttribute('scale', '.26 .26 .26');
+        //fish.setAttribute('scale', '.26 .26 .26');
         fish.setAttribute('material', 'src: ../models/fish.jpg');
     }
 
